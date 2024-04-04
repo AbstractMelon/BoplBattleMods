d7a69fb141a9de057a424edc8cc6e5b66ec6d5c3
AbstractMelon <129030233+AbstractMelon@users.noreply.github.com>
2024-04-03 20:47:53 -0500

    Create SplotchPlugin.cs

---
+++ NOTMINE/ExtinctionMetor/SplotchPlugin.cs
@@ -0,0 +1,35 @@
+using Splotch;
+using BoplFixedMath;
+using HarmonyLib;
+using System.Reflection;
+using UnityEngine;
+
+namespace ExtinctionMetor
+{
+    public class ExtinctionMetor : SplotchMod
+    {
+        public override void OnLoad()
+        {
+            Logger.Log("Plugin ExtinctionMetor is loaded!");
+
+            Harmony harmony = new Harmony("com.savaGe.ExtinctionMetor");
+
+
+            MethodInfo original = AccessTools.Method(typeof(MeteorSmash), "Awake");
+            MethodInfo patch = AccessTools.Method(typeof(myPatches), "Awake_ExtinctionMetor_Plug");
+            harmony.Patch(original, new HarmonyMethod(patch));
+        }
+    }
+    public class myPatches
+    {
+        public static void Awake_ExtinctionMetor_Plug(MeteorSmash __instance)
+        {
+            __instance.timeBeforeDrop = (Fix)0.1;
+            __instance.maxHoldTime = (Fix)2;
+            __instance.minHoldTime = (Fix)0.001;
+            __instance.vibrationStr = (Fix)1.5;
+            __instance.groundForce = (Fix)3000;
+            __instance.playerPushForce = (Fix)800;
+        }
+    }
+}
